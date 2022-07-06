17f217e0fc04a34dfd334b70bf90d98e4a0ed056
霽月 <kakky_parfait108@yahoo.co.jp>
2022-07-06 13:10:59 +0900

    7th commit.

--- next.config.js
+++ next.config.js
@@ -11,13 +11,19 @@ module.exports = nextConfig
  * @param {string} appEnv
  */
 function loadEnv(appEnv = "local") {
-  console.log(appEnv);
-  const env = {
-    ...require(`./env/env.${appEnv}`),
-    NEXT_PUBLIC_APP_ENV: appEnv,
-  };
+  try{
+    const env = {
+      ...require(`./env/env.${appEnv}`),
+      NEXT_PUBLIC_APP_ENV: appEnv,
+    };
 
-  Object.entries(env).forEach(([key, value]) => {
-    process.env[key] = value;
-  });
+    Object.entries(env).forEach(([key, value]) => {
+      process.env[key] = value;
+    });
+    
+  }catch(e){
+    if(appEnv == "local"){
+      process.env["BASE_URL"] = "http://localhost:3000";
+    }
+  }
 }
