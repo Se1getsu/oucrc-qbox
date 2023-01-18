7013170da582ac70ec44dc5e20734fe3b3bffd0c
temasaguru <temasaguru@gmail.com>
2023-01-18 16:58:19 +0900

    fix: 現時点ではmuiと互換性ないのでreact17にダウングレード

--- next.config.js
+++ next.config.js
@@ -3,26 +3,25 @@ loadEnv(process.env.APP_ENV);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
 
 /**
  * @param {string} appEnv
  */
 function loadEnv(appEnv = "local") {
-  try{
+  try {
     const env = {
-      ...require(`./env/env.${appEnv}`),
+      ...require(`./env/.env.${appEnv}`),
       NEXT_PUBLIC_APP_ENV: appEnv,
     };
 
     Object.entries(env).forEach(([key, value]) => {
       process.env[key] = value;
     });
-    
-  }catch(e){
-    if(appEnv == "local"){
+  } catch (e) {
+    if (appEnv == "local") {
       process.env["BASE_URL"] = "http://localhost:3000";
     }
   }
